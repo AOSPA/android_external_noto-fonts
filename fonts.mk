8f1ac18e382bd4aa08ef3b6ad5cfa41f68374009
Radosław Błędowski <rkbdi@aospa.co>
2024-07-23 03:05:41 +0530

    noto-fonts: Add Noto Emoji Addons
    Change-Id: Ie89d5dd58a373abd054b2e25408fb9baa1f20af4
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- fonts.mk
+++ fonts.mk
@@ -23,6 +23,7 @@ endif
 PRODUCT_PACKAGES := \
     $(PRODUCT_PACKAGES) \
     NotoColorEmoji.ttf \
+    NotoColorEmojiAddon.ttf \
     NotoColorEmojiFlags.ttf \
     NotoNaskhArabic-Bold.ttf \
     NotoNaskhArabic-Regular.ttf \
